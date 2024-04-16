91bd0879aaba4758e03d161f977d0efa4c6341d9
MichaelPotemkin <michael.potyomkin@gmail.com>
2024-04-16 18:07:00 +0100

    Added creds

--- config.py
+++ config.py
@@ -7,7 +7,11 @@ def get_credentials(file_name='credentials_config.json'):
     try:
         with open(file_name) as f:
             credentials_info = json.load(f)
-            credentials = Credentials.from_service_account_info(credentials_info)
+            scopes = [
+                'https://www.googleapis.com/auth/spreadsheets',
+                'https://www.googleapis.com/auth/drive'
+            ]
+            credentials = Credentials.from_service_account_info(credentials_info, scopes=scopes)
             return credentials
     except FileNotFoundError:
         print(f"{file_name} not found.")
